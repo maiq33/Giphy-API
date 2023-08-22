2ebcccb57b472e7f263a45b3945cf576025b35b9
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-08-22 03:25:11 +0000

    final del ejercicios

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
-function setup(){
+let api = "https://api.giphy.com/v1/gifs/search?"
+let apiKey = "api_key=qJy3unluYamCJQct7uphzWiZUSbvV4uN"
+let query = "&q=cosmic+cats"
+let limit = "&limit=25"
 
+function setup(){
+    noCanvas();
+    let url = api+apiKey+query+limit;
+    loadJSON(url, gotData);
 }
 
-function draw(){
+function gotData(giphy){
+    for (let i = 0; i < giphy.data.length; i++) {
+        createImg(giphy.data[i].images.original.url);
+                
+    }
     
 }
+
